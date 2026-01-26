bae0d78a9d59853088d733301228bafb522246a2
nath <karolinamariabodziony@gmail.com>
2026-01-26 12:25:47 +0100

    :(

--- js/app.js
+++ js/app.js
@@ -4,12 +4,15 @@ let makeCard = (p) => {
     let zdj = document.createElement("zdj");
     let info = document.createElement("info");
     let imie = document.createElement("imie");
-    imie.innerText = p.imie;
     let klan = document.createElement("klan");
-    klan.innerText = p.klan;
     let plec = document.createElement("plec");
     let ranga = document.createElement("ranga");
     let linki = document.createElement("linki");
+
+    imie.innerText = p.imie;
+    klan.innerText = p.klan;
+    plec.innerText = p.plec;
+    ranga.innerText = p.ranga;
 }
 
 document.addEventListener('DOMContentLoaded', async () => {
@@ -22,7 +25,7 @@ document.addEventListener('DOMContentLoaded', async () => {
         });
         const postacie = await res.json();
         if(postacie.valid) {
-            console.log(res.data);
+            console.log(postacie.data);
             postacie.data.forEach(p => {
                 makeCard(p);
             });
